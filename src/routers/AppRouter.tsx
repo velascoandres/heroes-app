417d4292266dfdf4c6e33ce6852b01b0c08136b7
Andrés Velasco <velasco.andrs@gmail.com>
2021-05-09 16:40:54 -0500

    add the second router

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,20 +1,15 @@
 import React, { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { LoginScreen } from '../components/login/LoginScreen';
-import { MarvelScreen } from '../components/marvel/MarvelScreen';
-import { Navbar } from '../components/ui/Navbar';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter: FC = () => {
     return (
         <Router>
             <div>
-                <Navbar />
-
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
                 <Switch>
                     <Route exact path="/login" component={LoginScreen}></Route>
-                    <Route exact path="/" component={MarvelScreen}></Route>
+                    <Route path="/" component={DashboardRoutes}></Route>
                 </Switch>
             </div>
         </Router>
---
+++ src/routers/DashboardRoutes.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { Fragment } from 'react';
+import { Navbar } from '../components/ui/Navbar';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { HeroScreen } from '../components/heroes/HeroScreen';
+import { DcSreen } from '../components/dc/DcSreen';
+
+export const DashboardRoutes: FC = () => {
+    return (
+        <Fragment>
+            <Navbar />
+
+            <div>
+                <Switch>
+                    <Route exact path="/marvel" component={MarvelScreen}></Route>
+                    <Route exact path="/heroe/:heroeId" component={HeroScreen}></Route>
+                    <Route exact path="/dc" component={DcSreen}></Route>
+                    
+                    <Redirect to="/marvel" />
+                </Switch>
+            </div>
+        </Fragment>
+    );
+};
