4914c7bce4982d2a4cc4e3618112983dce5404c0
Andrés Velasco <velasco.andrs@gmail.com>
2021-05-10 22:09:55 -0500

    hero screen

--- src/components/heroes/HeroScreen.tsx
+++ src/components/heroes/HeroScreen.tsx
@@ -1,13 +1,18 @@
 import React, { FC } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
+import { History, LocationState } from 'history';
+
 import { getHeroeById } from '../../selectors/getHeroById';
-import { IHero } from './interfaces';
 
 export interface HeroParams {
     heroId: string;
 }
 
-export const HeroScreen: FC = () => {
+interface HeroScreenProps {
+    history: History<LocationState>;
+}
+
+export const HeroScreen: FC<HeroScreenProps> = ({ history }: HeroScreenProps) => {
     const { heroId } = useParams<HeroParams>();
 
     const hero = getHeroeById(heroId);
@@ -18,11 +23,60 @@ export const HeroScreen: FC = () => {
         return <Redirect to="/" />;
     }
 
-    const {superhero, characters, alter_ego, publisher, first_appearance} = hero;
+    const handleReturn = () => {
+        const { goBack, length, push } = history;
+        if (length <= 2) {
+            push('/');
+        } else {
+            goBack();
+        }
+    };
+
+    const { id, superhero, characters, alter_ego, publisher, first_appearance } = hero;
 
     return (
-        <div>
-            <h2>Hero: {superhero}</h2>
+        <div className="row mt-5">
+            <div className="col-md-4 col-sm-12">
+                <img src={`/assets/heroes/${id}.jpg`} className="card-img-top" alt={superhero} />{' '}
+            </div>
+
+            <div className="col-md-8 col-sm-12">
+                <h2>Hero: {superhero} </h2>
+                <span className="badge bg-secondary ms-1">{publisher}</span>
+                <hr />
+
+                <p className="text">
+                    <strong>Alter Ego: </strong>
+                    {alter_ego}
+                </p>
+
+                {alter_ego !== characters && (
+                    <>
+                        <hr />
+                        <div className="card-text">
+                            {characters.split(',').map((character: string, index: number) => {
+                                const style = index % 2 === 0 ? 'bg-primary' : 'bg-success';
+
+                                return (
+                                    <span key={character} className={`badge ${style} ms-1`}>
+                                        {character}
+                                    </span>
+                                );
+                            })}
+                        </div>
+                    </>
+                )}
+
+                <hr />
+
+                <p>
+                    <strong>First Appereace: </strong> <small className="text-muted">{first_appearance}</small>
+                </p>
+
+                <button className="btn btn-success" onClick={handleReturn}>
+                    Return
+                </button>
+            </div>
         </div>
     );
 };
